74fcf9522b62c28e0b0d516c71e0fa025151c875
sajakshrestha8 <shresthasajak123@gmail.com>
2024-09-28 16:05:27 +0545

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/verify", (req, res) => {
+  const { code } = req.body;
+
+  if (!code || code.length !== 6 || code[5] === "7") {
+    return res.status(400).json({ message: "Invalid code" });
+  }
+
+  return res.status(200).json({ message: "Code successfully verified" });
+});
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
